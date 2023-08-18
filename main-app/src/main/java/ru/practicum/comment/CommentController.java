bbca11203b581eae525321870753766c9e681322
AlexeiIll <linedaykek@gmail.com>
2023-08-18 08:52:42 +0300

    fix: due to checkstyle errors

--- main-app/src/main/java/ru/practicum/comment/CommentController.java
+++ main-app/src/main/java/ru/practicum/comment/CommentController.java
@@ -23,7 +23,7 @@ import java.util.List;
 @Slf4j
 public class CommentController {
 
-    private final static String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     private final CommentService commentService;
 
     @PostMapping("/users/{userId}/events/{eventId}/comments")
